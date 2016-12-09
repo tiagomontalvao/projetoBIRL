18ab3ed08676d1ddc6de5606a53f22cabf76e400
Igor Carpanese <carpanese.igor@gmail.com>
2016-12-09 21:53:42 -0200

    Revert no human_player.

--- models/players/human_player.py
+++ models/players/human_player.py
@@ -1,16 +1,19 @@
 from models.move import Move
-class HumanPlayer:
-  def __init__(self, color):
-    self.color = color
 
-  def play(self, board):
-    for idx, move in enumerate(board.valid_moves(self.color)):
-      print str(idx) + '. [' + str(move.x) + ', ' + str(move.y) + ']'
-    rowInp = int(raw_input("Linha: "))
-    colInp = int(raw_input("Coluna: "))
-    inp = int(raw_input("Jogada: "))
-    while inp < 0 or inp >= board.valid_moves(self.color).__len__():
-      print board
-      print "Movimento invalido. Insira um valido."
-      inp = int(raw_input("Jogada: "))
-    return board.valid_moves(self.color)[inp]
+class HumanPlayer:
+	def __init__(self, color):
+		self.color = color
+
+	def play(self, board):
+		rowInp = int(raw_input("Linha: "))
+		colInp = int(raw_input("Coluna: "))
+		move = Move(rowInp, colInp)
+
+		while move not in board.valid_moves(self.color):
+			print "Movimento invalido.Insira um valido"
+			print board
+			rowInp = int(raw_input("Linha: "))
+			colInp = int(raw_input("Coluna: "))
+			move = Move(rowInp, colInp)
+
+		return move
